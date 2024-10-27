c094bd462f3d94fa0aa06a233820f04e31c7eb97
Aito <tsareva.na2k@gmail.com>
2024-10-27 17:21:07 +0300

    everything about activity

---
+++ app/src/main/java/com/example/ask_tsareva/MyActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ask_tsareva
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MyActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
